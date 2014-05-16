c97e98885102daa1456abecc21eda341a3a24f08
Andrew Graham <andrew.t.graham@live.com>
2014-05-15 21:11:38 -0700

    initial commit
    Client side implementation of twentyQ

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyQclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Client[] clients = new Client[5];
+            for(int i = 0; i < clients.Length; i++)
+            {
+                clients[i] = new Client();
+                clients[i].port = 14000;
+                clients[i].IP = "127.0.0.1";
+                clients[i].connect();
+                Console.WriteLine("Client {0} connected to server on port: {0}", i, clients[i].port);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace twentyQclient
+{
+    public class Client
+    {
+        public int port;
+        public string IP;
+        private TcpClient client;
+
+        public void connect()
+        {
+            client = new TcpClient();
+            client.Connect(IP, port);
+        } 
+    }
+}
